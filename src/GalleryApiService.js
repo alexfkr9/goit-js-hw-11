47ee5412a0d5bf27282055139729378b66dd330b
Alexfkr <alexfkr9@gmail.com>
2022-06-30 15:40:48 +0300

    task

--- src/GalleryApiService.js
+++ src/GalleryApiService.js
@@ -1,52 +1,61 @@
 import axios from 'axios';
 
 export default class NewsApiService {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-        this.currentCard = 1;
-    } 
-  
-    fetchArticles = async () =>  {
-        
-        const headers = new Headers({
-          "Content-Type": "application/json",
-          "X-Custom-Header": "custom value",
-        });
-        const url = `https://pixabay.com/api/?key=28304018-265b00fbf5f9e6bf82ef29498&q=${this.searchQuery}&
-                  image_type=photo&orientation=horizontal&safesearch=false&page=${this.page}&per_page=40
-                  `;
-        
-        const fetchGallery = await axios.get(url, headers);
-        
-        this.incrementPage();
-        this.incrementCard();
-
-        return fetchGallery.data;          
-    }
-  
-    incrementPage() {
-        this.page += 1
-    }
-  
-    incrementCard() {
-        this.currentCard += 40;
-    }
-
-    resetPageAndCard() {
-        this.page = 1;
-        this.currentCard = 1;        
-    }    
-
-    getCurrentCard() {
-        return this.currentCard;
-    }    
-  
-    get query() {
-        return this.searchQuery;
-    }
-  
-    set query(newQuery) {
-        this.searchQuery = newQuery;
-    }
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.currentCard = 1;
   }
+
+  fetchArticles = async () => {
+    const PER_PAGE = 40;
+
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+    });
+
+    const searchParams = new URLSearchParams({
+      key: '28304018-265b00fbf5f9e6bf82ef29498',
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: false,
+      page: this.page,
+      per_page: PER_PAGE,
+    });
+
+    const url = `https://pixabay.com/api/?${searchParams}`;
+
+    const fetchGallery = await axios.get(url, headers);
+
+    this.incrementPage();
+    this.incrementCard();
+
+    return fetchGallery.data;
+  };
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  incrementCard() {
+    this.currentCard += 40;
+  }
+
+  resetPageAndCard() {
+    this.page = 1;
+    this.currentCard = 1;
+  }
+
+  getCurrentCard() {
+    return this.currentCard;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './css/styles.css';
+import './css/styles.scss';
 import GalleryApiService from './GalleryApiService.js';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -8,20 +8,18 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryApiService = new GalleryApiService();
 
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
-const searchForm = document.querySelector("#search-form");
-const gallery = document.querySelector(".gallery");
-const loadMoreBtn = document.querySelector(".load-more");
-
-searchForm.addEventListener('submit', onSearch)
-
+searchForm.addEventListener('submit', onSearch);
 
 function onSearch(e) {
   e.preventDefault();
 
-  clearMarkup();    
+  clearMarkup();
 
-  galleryApiService.query = e.currentTarget.elements.searchQuery.value;  
+  galleryApiService.query = e.currentTarget.elements.searchQuery.value;
 
   loadMoreBtn.classList.add('is-hidden');
 
@@ -30,69 +28,84 @@ function onSearch(e) {
 
 function clearMarkup() {
   gallery.innerHTML = '';
-  galleryApiService.resetPageAndCard();  
+  galleryApiService.resetPageAndCard();
 }
 
-
 function chooseMarkup(data) {
-  console.log(data);
-  
   if (data.hits.length === 0) {
-      Notify.failure('Sorry, there are no images matching your search query. Please try again.');    
-      return;
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
   }
   Notify.success(`Hooray! We found ${data.totalHits} images.`);
   addMarkup(data);
-  
 }
 
 function addMarkup(data) {
-  const cards=data.hits;
+  const cards = data.hits;
   const markup = cards
     .map(card => {
       return `<div class="gallery__item">
-                <a class="" href='${card.largeImageURL}'>              
+                <a class="gallery__link" href='${card.largeImageURL}'>              
                   <img class="gallery__image" src='${card.webformatURL}' alt='${card.pageURL}' loading="lazy" />
                 </a>
                 <div class="info">
-                    <p class="info-item">
+                    <p class="info__item">
                       <b>Likes</b>
                       <span>${card.likes}</span>
                     </p>
-                    <p class="info-item">
+                    <p class="info__item">
                       <b>Views</b>
                       <span>${card.views}</span>
                     </p>
-                    <p class="info-item">
+                    <p class="info__item">
                       <b>Comments</b>
                       <span>${card.comments}</span>
                     </p>
-                    <p class="info-item">
+                    <p class="info__item">
                       <b>Downloads</b>
                       <span>${card.downloads}</span>
                     </p>
                 </div>    
-              </div>`               
-      ;
+              </div>`;
     })
     .join('');
-    gallery.innerHTML = markup;
 
-    var lightbox = new SimpleLightbox(".gallery a", {});
+  // gallery.innerHTML = markup;    // For switch page
+  gallery.insertAdjacentHTML('beforeend', markup);
 
-    if ( galleryApiService.getCurrentCard() >= data.totalHits) { 
-          loadMoreBtn.classList.add('is-hidden');     
-          Notify.info('We\'re sorry, but you\'ve reached the end of search results.');    
-          return;
-    }
+  loadMoreBtn.classList.remove('is-hidden');
+  loadMoreBtn.addEventListener('click', onLoadMore);
 
-    loadMoreBtn.classList.remove('is-hidden');  
-    loadMoreBtn.addEventListener('click', onLoadMore);
-    lightbox.refresh();
+  // Smooth page scrolling
+  pageScrolling();
+
+  // SimpleLightbox
+  var lightbox = new SimpleLightbox('.gallery a', {});
+  lightbox.refresh();
+
+  // notify the end of search results
+  if (galleryApiService.getCurrentCard() >= data.totalHits) {
+    loadMoreBtn.classList.add('is-hidden');
+    Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+    return;
+  }
 }
-
 
 function onLoadMore() {
   galleryApiService.fetchArticles().then(addMarkup);
 }
 
+function pageScrolling() {
+  const { height: cardHeight } = document
+    .querySelector('.scroll-window')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight,
+    behavior: 'smooth',
+  });
+}
